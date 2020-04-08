cb59f261e7789ba2cf16f590087d0440cd99708e
Branko Karac <karacbranko@gmail.com>
2020-04-08 18:28:12 +0200

    #PITZ-16 events and listeners added

---
+++ app/Listeners/NewMovieIsCreatedListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\NewMovieIsCreatedEvent;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Mail\MovieCreated;
+use Illuminate\Support\Facades\Mail;
+
+
+
+class NewMovieIsCreatedListener
+{
+    
+
+    /**
+     * Handle the event.
+     *
+     * @param  NewMovieIsCreatedEvent  $event
+     * @return void
+     */
+    public function handle(NewMovieIsCreatedEvent $event)
+    {
+        Mail::to('test@test.com')->send(new MovieCreated($event->movie));
+        
+    }
+}
